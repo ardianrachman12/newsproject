11e87aa9c73104a9a5810d3b5d5cdf6913dd16ec
Mohamad Syifa Ardian Rachman <ardianrachman9@gmail.com>
2025-10-06 21:44:46 +0700

    [update] src img

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
+import logo from "../../../src/assets/images/logo.svg";
 
 const Header = () => {
   return (
     <header className="text-white p-4 text-center">
-      <img src="/src/assets/images/logo.svg" alt="Logo" className="mx-auto py-6" />
+      <img src={logo} alt="Logo" className="mx-auto py-6" />
       <Navbar />
     </header>
   );
--- src/components/news/Category.jsx
+++ src/components/news/Category.jsx
@@ -1,29 +1,33 @@
 import React from "react";
+import healthImg from "../../../src/assets/images/category-health.png";
+import techImg from "../../../src/assets/images/category-technology.png";
+import sportImg from "../../../src/assets/images/category-sport.png";
+import worldImg from "../../../src/assets/images/category-worldnews.png";
 
 const CategoryNews = () => {
   const categories = [
     {
       name: "Health",
       id: "health",
-      img: "/src/assets/images/category-health.png",
+      img: healthImg,
       description: "Latest updates on health and wellness.",
     },
     {
       name: "Technology",
       id: "technology",
-      img: "/src/assets/images/category-technology.png",
+      img: techImg,
       description: "Latest updates on technology and innovation.",
     },
     {
       name: "Sports",
       id: "sports",
-      img: "/src/assets/images/category-sport.png",
+      img: sportImg,
       description: "Latest updates on sports and athletics.",
     },
     {
       name: "World News",
       id: "world-news",
-      img: "/src/assets/images/category-worldnews.png",
+      img: worldImg,
       description: "Latest updates on world news and global events.",
     },
   ];
@@ -32,7 +36,7 @@ const CategoryNews = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {categories.map((category) => (
         <div key={category.id} className="flex gap-x-[10px] items-center">
-          <img src={category.img} alt="" className="" />
+          <img src={category.img} alt={category.name} />
           <div className="flex flex-col gap-2">
             <h3 className="text-[15px] text-left font-semibold">{category.name}</h3>
             <p className="text-gray-400 text-xs text-left">{category.description}</p>
